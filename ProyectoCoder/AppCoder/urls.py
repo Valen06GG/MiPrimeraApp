068b68f2e57708a3c4d39267dc9fe72989689daf
Valentin Fortunato <valenfortunato06@gmail.com>
2025-06-04 15:28:50 -0300

    PrimeraApp

--- ProyectoCoder/AppCoder/urls.py
+++ ProyectoCoder/AppCoder/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import lista_estudiantes, detalle_estudiante
+from .views import lista_estudiantes, detalle_estudiante, cursos, profesores, estudiantes, entregables, inicio
+from AppCoder import views
 
 urlpatterns = [
-    path('estudiantes/', lista_estudiantes, name='lista_estudiantes'),
-    path('estudiantes/<int:pk>/', detalle_estudiante, name='detalle_estudiante'),
+    #path('estudiantes/', lista_estudiantes, name='lista_estudiantes'),
+    #path('estudiantes/<int:pk>/', detalle_estudiante, name='detalle_estudiante'),
+    path('', views.inicio),
+    path('cursos/', views.cursos, name='cursos'),
+    path('profesores/', views.profesores),
+    path('estudiantes/', views.estudiantes),
+    path('entregables', views.entregables),
 ]
--- ProyectoCoder/AppCoder/views.py
+++ ProyectoCoder/AppCoder/views.py
@@ -10,3 +10,18 @@ def lista_estudiantes(request):
 def detalle_estudiante(request, pk):
     estudiante = get_object_or_404(Estudiante,pk=pk)
     return render(request, 'AppCoder/estudiante_detail.html', {'estudiante': estudiante})
+
+def inicio(request):
+    return render(request, "AppCoder/index.html")
+
+def cursos(request):
+    return render(request, "AppCoder/cursos.html")
+
+def profesores(request):
+    return render(request, "AppCoder/profesores.html")
+
+def estudiantes(request):
+    return render(request, "AppCoder/estudiantes.html")
+
+def entregables(request):
+    return render(request, "AppCoder/entregables.html")
